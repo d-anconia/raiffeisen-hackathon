7119282e29616110f1ae0168b005fa5b5d2231ef
Ivan Shafran <shafran.fun@gmail.com>
2019-09-14 14:59:31 +0300

    Init di

---
+++ Android/app/src/main/java/com/github/ivanshafran/raif/App.kt
@@ -0,0 +1,13 @@
+package com.github.ivanshafran.raif
+
+import android.app.Application
+import com.github.ivanshafran.raif.di.ToothpickInitializer
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        ToothpickInitializer.initialize()
+    }
+
+}
